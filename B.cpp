36e7687ce3e1aadc50242af58adb028ec8a6661a
Koliopoulos, Anastasis <ice22390097@uniwa.gr>
2026-05-10 17:13:56 +0300

    Solved B.

--- B.cpp
+++ B.cpp
@@ -2,45 +2,50 @@
 #include <vector>
 #include <algorithm>
 #define MAXN 100000
+#define INF 2000000000
 using namespace std;
 
-struct presentation 
+struct presentation
 {
     int S;
     int E;
 };
 
-bool comparePresentations(presentation A, presentation B) 
-{ 
-    return A.S < B.S; 
+bool comparePresentations(presentation A, presentation B)
+{
+    return A.S < B.S;
 }
 
 vector<presentation> P(MAXN + 2);
 int N;
 int T;
 
-int main() 
+int main()
 {
     scanf("%d %d", &N, &T);
-    
+
     for (int i = 0; i < N; ++i)
     {
         scanf("%d %d", &P[i].S, &P[i].E);
     }
-    
-    P[N] = {0, 0};
-    P[N + 1] = {T, T};
-    
+
+    P[N] = { -INF, 0 };
+    P[N + 1] = { T, INF };
+
     sort(P.begin(), P.begin() + N + 2, comparePresentations);
-    
+
     int largestGap = 0;
     int maximumEndTime = P[0].E;
-    
-    for (int i = 1; i <= N + 1; ++i) 
+
+    for (int i = 1; i <= N + 1; ++i)
     {
-        largestGap = max(largestGap, P[i].S - maximumEndTime);
+        if (P[i].S != P[i - 1].S)
+        {
+            largestGap = max(largestGap, max(P[i].S - maximumEndTime,0));
+        }
+        
         maximumEndTime = max(maximumEndTime, P[i].E);
     }
-    
+
     printf("%d\n", largestGap);
 }
